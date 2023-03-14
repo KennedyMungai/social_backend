91873bcb511918a73bbe3cbf0c4c21250e77aa1c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-14 13:37:08 +0300

    Updated the spelling of the parameter

--- main.py
+++ main.py
@@ -30,11 +30,12 @@ async def root() -> dict:
 
 
 @app.post("/createpost")
-async def create_post(_new_post: Post) -> dict:
+async def create_post(_post: Post) -> dict:
     """A dummy endpoint to create a post
 
     Returns:
         dict: The message to show successful logic execution
     """
-    print(_new_post.rating)
-    return {"data": "new post"}
+    # print(_new_post.rating)
+    _post_dict = _post.dict()
+    return {"data": _post_dict}
