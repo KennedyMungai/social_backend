bb2171c071a0170ec31e56220e4e246bc112af44
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 04:32:02 +0300

    Added a database connection string for sqlalchemy

--- app/database.py
+++ app/database.py
@@ -1,4 +1,16 @@
 """A file that contains the database logic for the project"""
+import os
+
+from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+ENV = load_dotenv(find_dotenv())
+
+MYSQL_USERNAME = os.environ.get("MYSQL_USER")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
+
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}!@localhost:3306/{MYSQL_DATABASE}"
