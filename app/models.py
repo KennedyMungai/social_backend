a261852a09fbde1ad70ad2e53b61c294a7649dfc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 04:54:32 +0300

    Added a docstring to the Post class

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from sqlalchemy import Column, Integer, String, Boolean
 
 
 class Post(Base):
+    """The Post template
+
+    Args:
+        Base (Class): The parent class for the Post class
+    """
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
