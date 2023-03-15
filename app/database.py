d7336cf0e92d020adbd440394ffc32b070fbc020
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 04:24:21 +0300

    Added imports for various sqlalchemy methods

--- app/database.py
+++ app/database.py
@@ -1,1 +1,4 @@
 """A file that contains the database logic for the project"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
