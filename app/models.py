21e880415a89ba2819378c0d61f8ac81a98ca413
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 04:59:09 +0300

    Gave the published column a default value of true

--- app/models.py
+++ app/models.py
@@ -13,5 +13,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean)
+    published = Column(Boolean, default=True)
     rating = Column(Integer)
