d8988d018d4e841e1a84b10c3cf7146f27296cff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 04:53:05 +0300

    Created the model for the Post

--- app/models.py
+++ app/models.py
@@ -1,2 +1,12 @@
 """The script that contains all our model definitions"""
 from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean)
+    rating = Column(Integer)
